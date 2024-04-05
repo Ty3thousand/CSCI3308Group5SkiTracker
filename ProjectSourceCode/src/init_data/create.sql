559f898cf641b73e6335ebd39bc23f779f4cd666
Ty3thousand <111723679+Ty3thousand@users.noreply.github.com>
2024-04-04 18:26:28 -0600

    Update create.sql order of tables

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -5,20 +5,11 @@ CREATE TABLE IF NOT EXISTS users
     password VARCHAR(50) NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS user_to_ski_day
+CREATE TABLE IF NOT EXISTS reviews
 (
-    username VARCHAR(50) NOT NULL,
-    ski_day_id INT NOT NULL,
-    FOREIGN KEY (username) REFERENCES user (username) ON DELETE CASCADE,
-    FOREIGN KEY (ski_day_id) REFERENCES ski_day (ski_day_id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS ski_day
-(
-    ski_day_id SERIAL PRIMARY KEY NOT NULL,
-    mountain_name VARCHAR(50) NOT NULL,
-    top_speed DECIMAL NOT NULL,
-    FOREIGN KEY (mountain_name) REFERENCES mountain (mountain_name) ON DELETE CASCADE
+    review_id SERIAL PRIMARY KEY NOT NULL,
+    description VARCHAR(400) NOT NULL,
+    rating DECIMAL NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS mountains
@@ -28,18 +19,26 @@ CREATE TABLE IF NOT EXISTS mountains
     location VARCHAR(50)
 );
 
+CREATE TABLE IF NOT EXISTS ski_day
+(
+    ski_day_id SERIAL PRIMARY KEY NOT NULL,
+    mountain_name VARCHAR(50) NOT NULL,
+    top_speed DECIMAL NOT NULL,
+    FOREIGN KEY (mountain_name) REFERENCES mountains (mountain_name) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS user_to_ski_day
+(
+    username VARCHAR(50) NOT NULL,
+    ski_day_id INT NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE,
+    FOREIGN KEY (ski_day_id) REFERENCES ski_day (ski_day_id) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS mountains_to_reviews
 (
     mountain_name VARCHAR(50),
     review_id INT NOT NULL,
-    FOREIGN KEY (mountain_name) REFERENCES mountain (mountain_name) ON DELETE CASCADE,
-    FOREIGN KEY (review_id) REFERENCES review (review_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE IF NOT EXISTS reviews
-(
-    review_id SERIAL PRIMARY KEY NOT NULL,
-    description VARCHAR(400) NOT NULL,
-    rating DECIMAL NOT NULL
+    FOREIGN KEY (mountain_name) REFERENCES mountains (mountain_name) ON DELETE CASCADE,
+    FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE
 );
