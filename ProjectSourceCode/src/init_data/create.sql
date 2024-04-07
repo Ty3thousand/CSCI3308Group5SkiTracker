97dab53dd42b90d436fa2f6e094c2a95887a7db1
anne6808 <anne6808@colorado.edu>
2024-04-06 20:27:02 -0600

    Base Reviews Page Functionality

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users
 (
     username VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -5,6 +6,15 @@ CREATE TABLE IF NOT EXISTS users
     password VARCHAR(50) NOT NULL
 );
 
+DROP TABLE IF EXISTS reviews CASCADE;
+CREATE TABLE IF NOT EXISTS reviews
+(
+    review_id SERIAL PRIMARY KEY NOT NULL,
+    description VARCHAR(400) NOT NULL,
+    rating DECIMAL NOT NULL
+);
+
+DROP TABLE IF EXISTS mountains CASCADE;
 CREATE TABLE IF NOT EXISTS mountains
 (
     mountain_name VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -12,6 +22,7 @@ CREATE TABLE IF NOT EXISTS mountains
     location VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS ski_day CASCADE;
 CREATE TABLE IF NOT EXISTS ski_day
 (
     ski_day_id SERIAL PRIMARY KEY NOT NULL,
@@ -20,6 +31,7 @@ CREATE TABLE IF NOT EXISTS ski_day
     FOREIGN KEY (mountain_name) REFERENCES mountains (mountain_name) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS user_to_ski_day CASCADE;
 CREATE TABLE IF NOT EXISTS user_to_ski_day
 (
     username VARCHAR(50) NOT NULL,
@@ -28,7 +40,7 @@ CREATE TABLE IF NOT EXISTS user_to_ski_day
     FOREIGN KEY (ski_day_id) REFERENCES ski_day (ski_day_id) ON DELETE CASCADE
 );
 
-
+DROP TABLE IF EXISTS mountains_to_reviews CASCADE;
 CREATE TABLE IF NOT EXISTS mountains_to_reviews
 (
     mountain_name VARCHAR(50),
@@ -36,5 +48,3 @@ CREATE TABLE IF NOT EXISTS mountains_to_reviews
     FOREIGN KEY (mountain_name) REFERENCES mountains (mountain_name) ON DELETE CASCADE,
     FOREIGN KEY (review_id) REFERENCES reviews (review_id) ON DELETE CASCADE
 );
-
-
