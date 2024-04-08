4741e90932fa4c20a1a35fe2318f488b4196edeb
mash8635 <mash8635@colorado.edu>
2024-04-08 12:54:57 -0600

    max register page and js function to verify password

---
+++ ProjectSourceCode/src/resources/js/script.js
@@ -0,0 +1,11 @@
+
+
+function check() {
+    var input = document.getElementById('password_confirm');
+    if (input.value != document.getElementById('password').value) {
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        // input is valid -- reset the error message
+        input.setCustomValidity('');
+    }
+}
